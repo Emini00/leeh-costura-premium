e7cec4b7f357fecc05931493660ed135a69c620a
Victor Santos <victorluis_lucas@hotmail.com>
2026-05-18 13:39:37 -0300

    correção na ordem das migrate

--- database/migrations/2026_05_07_193800_create_fabricas_table.php
+++ database/migrations/2026_05_07_193800_create_fabricas_table.php
@@ -8,16 +8,17 @@ return new class extends Migration {
     /**
      * Run the migrations.
      */
-   public function up(): void
-{
-    //
-}
+    public function up(): void
+    {
+        Schema::create('fabricas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->timestamps();
+        });
+    }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-      //
+        Schema::dropIfExists('fabricas');
     }
 };
